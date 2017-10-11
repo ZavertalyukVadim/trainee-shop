353e1ec937842311ee9bd0c9020e483d33a077bf
vadimka <zavertalyuk.v@gmail.com>
2017-10-11 14:37:59 +0300

    change update method and add logs

--- src/main/java/com/example/demo/services/GoodsService.java
+++ src/main/java/com/example/demo/services/GoodsService.java
@@ -2,6 +2,8 @@ package com.example.demo.services;
 
 import com.example.demo.dao.GoodsDao;
 import com.example.demo.entities.Goods;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +12,7 @@ import java.util.List;
 @Service
 public class GoodsService {
     private final GoodsDao goodsDao;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     public GoodsService(GoodsDao goodsDao) {
@@ -17,37 +20,39 @@ public class GoodsService {
     }
 
 
-    public List<Goods> getAllGoods(){
+    public List<Goods> getAllGoods() {
 
         return goodsDao.findAll();
     }
 
-    public Goods getGoodsById(Integer id){
+    public Goods getGoodsById(Integer id) {
         return goodsDao.findOne(id);
     }
 
-    public Integer createGoods(Goods goods){
+    public Integer createGoods(Goods goods) {
         return goodsDao.save(goods).getId();
     }
 
-    public boolean updateGoods(Integer id,Goods goods){
-        try {
-            Goods newGoods = goodsDao.findOne(id);
-            newGoods.setName(goods.getName());
-            newGoods.setType(goods.getType());
-            newGoods.setVendor(goods.getVendor());
-            goodsDao.save(newGoods);
+    public boolean updateGoods(Integer id, Goods goods) {
+        Goods newGoods = goodsDao.findOne(id);
+        logger.debug("check if goods with id " + id + " exists in database");
+        if (newGoods != null) {
+            logger.debug("Update goods with input id = " + id);
+            goods.setId(id);
+            goodsDao.save(goods);
             return true;
-        }catch (Exception e){
+        } else {
+            logger.debug("attempt to update goods with nonexistent id = " + id);
             return false;
         }
+
     }
 
-    public boolean deleteGoodsById(Integer id){
+    public boolean deleteGoodsById(Integer id) {
         try {
             goodsDao.delete(id);
             return true;
-        }catch (Exception e){
+        } catch (Exception e) {
             return false;
         }
     }
