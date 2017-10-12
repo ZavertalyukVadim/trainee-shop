ac2080f62fd9d36aa570441bb886172ac9bf51fe
vadimka <zavertalyuk.v@gmail.com>
2017-10-12 15:29:09 +0300

    add two test for delete and add test client

--- src/test/java/com/example/demo/controllers/ClientControllerTest.java
+++ src/test/java/com/example/demo/controllers/ClientControllerTest.java
@@ -1,6 +1,6 @@
 package com.example.demo.controllers;
 
-import com.example.demo.entities.Client;
+import com.example.demo.entities.*;
 import com.example.demo.services.ClientService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
@@ -18,9 +18,13 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -43,11 +47,45 @@ public class ClientControllerTest {
     @Autowired
     private WebApplicationContext wac;
 
+    public Client test() {
+        Client client = new Client("name");
+
+        client.setId(1);
+        Vendor vendor = new Vendor();
+        vendor.setName("vendor");
+
+        vendor.setId(1);
+        Goods goods = new Goods();
+        goods.setName("goods name");
+        goods.setType(Type.COMPUTER);
+        goods.setVendor(vendor);
+        goods.setId(1);
+        List<Goods> goodsList = new ArrayList<>();
+        goodsList.add(goods);
+
+        vendor.setGoods(goodsList);
+
+
+        Order order = new Order();
+        order.setName("order");
+        order.setOrderItems(Arrays.asList(
+                new OrderItem(1,goods, 5),
+                new OrderItem(2,null,5)
+        ));
+        order.setClient(
+                client
+        );
+        order.setId(1);
+        client.setOrders(Arrays.asList(order));
+        return client;
+    }
+
 
     @Test
     public void getClientById() throws Exception {
         ObjectMapper mapper = new ObjectMapper();
-        Client client = clientService.getClientById(1);
+        Client client = test();
+
 
         MvcResult mvcResult = this.mockMvc.perform(get("/client/1")
                 .accept(MediaType.parseMediaType("application/json;charset=UTF-8"))
@@ -67,6 +105,30 @@ public class ClientControllerTest {
         assertThat(mvcResult.getResponse().getStatus(), is(HttpServletResponse.SC_NOT_FOUND));
 
     }
+
+    @Test
+    public void deleteClientById() throws Exception {
+        MvcResult mvcResult = this.mockMvc.perform(delete("/client/1")
+                .accept(MediaType.parseMediaType("application/json;charset=UTF-8"))
+                .content("application/json;charset=UTF-8"))
+                .andReturn();
+
+        assertThat(mvcResult.getResponse().getStatus(), is(HttpServletResponse.SC_ACCEPTED));
+
+    }
+
+    @Test
+    public void deleteClientByNonexistentId() throws Exception {
+        MvcResult mvcResult = this.mockMvc.perform(delete("/client/100000")
+                .accept(MediaType.parseMediaType("application/json;charset=UTF-8"))
+                .content("application/json;charset=UTF-8"))
+                .andReturn();
+
+        assertThat(mvcResult.getResponse().getStatus(), is(HttpServletResponse.SC_NOT_FOUND));
+
+    }
+
+
 
 }
 
