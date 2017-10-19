1d68c462729b00df5625fe66c013d6df448a6fcc
vadimka <zavertalyuk.v@gmail.com>
2017-10-19 09:20:24 +0300

    connect methods from dao with methods on controller

--- src/main/java/com/example/demo/services/VendorService.java
+++ src/main/java/com/example/demo/services/VendorService.java
@@ -22,49 +22,44 @@ public class VendorService {
     public List<Vendor> getAllVendors() {
         logger.info("attempt to get all vendors");
         return vendorDao.findAll();
-//        return null;
     }
 
     public Vendor getVendorById(Integer id) {
         logger.info("attempt to get vendor with id = " + id);
         return vendorDao.findOne(id);
-//        return null;
     }
 
     public Integer createVendor(Vendor vendor) {
         logger.info("attempt to create vendor");
-//        try {
-//            return vendorDao.save(vendor).getId();
-//        } catch (Exception e) {
-//            return 0;
-//        }
-        return 0;
+        try {
+            return vendorDao.save(vendor).getId();
+        } catch (Exception e) {
+            return 0;
+        }
     }
 
     public boolean updateVendor(Integer id, Vendor vendor) {
         logger.info("attempt to update vendor with id = " + id);
-//        Vendor newVendor = vendorDao.findOne(id);
+        Vendor newVendor = vendorDao.findOne(id);
         logger.debug("check if vendor with id " + id + " exists in database");
-//        if (newVendor != null) {
-//            logger.debug("Update vendor with input id = " + id);
-//            vendor.setId(id);
-//            vendorDao.save(vendor);
-//            return true;
-//        } else {
-//            logger.debug("attempt to update vendor with nonexistent id = " + id);
-//            return false;
-//    }
-        return false;
+        if (newVendor != null) {
+            logger.debug("Update vendor with input id = " + id);
+            vendor.setId(id);
+            vendorDao.save(vendor);
+            return true;
+        } else {
+            logger.debug("attempt to update vendor with nonexistent id = " + id);
+            return false;
+        }
     }
 
     public boolean deleteVendorById(Integer id) {
-//        logger.info("attempt to delete vendor with id = " + id);
-//        try {
-//            vendorDao.delete(id);
-//            return true;
-//        } catch (Exception e) {
-//            return false;
-//        }
-        return false;
+        logger.info("attempt to delete vendor with id = " + id);
+        try {
+            vendorDao.delete(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
