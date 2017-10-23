09ffa2d2f6960adbbbf3bac03c2a2f56186912ab
vadimka <zavertalyuk.v@gmail.com>
2017-10-23 10:11:22 +0300

    add dao and service for vendor

--- src/main/java/com/example/demo/dao/VendorDao.java
+++ src/main/java/com/example/demo/dao/VendorDao.java
@@ -1,7 +1,87 @@
 package com.example.demo.dao;
 
+import com.example.demo.entities.Vendor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public class VendorDao {
+    private final SessionFactory sessionFactory;
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    public VendorDao(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Transactional
+    public List<Vendor> getAllVendors() {
+        Session session = sessionFactory.openSession();
+        return session.createQuery("SELECT v from Vendor v").list();
+    }
+
+    @Transactional
+    public Vendor getVendorById(Integer id) {
+        Session session = sessionFactory.openSession();
+        try {
+            logger.trace("try to get vendor with id=" + id);
+            return session.byId(Vendor.class).load(id);
+        } catch (Exception e) {
+            logger.info(e.getMessage());
+            return null;
+        }
+    }
+
+    @Transactional
+    public Integer createVendor(Vendor vendor) {
+        Session session = sessionFactory.openSession();
+        vendor.setId(null);
+        try {
+            logger.trace("try to create vendor");
+            return ((Integer) session.save(vendor));
+        } catch (Exception e) {
+            logger.info(e.getMessage());
+            return 0;
+        }
+    }
+
+    @Transactional
+    public boolean updateVendor(Integer id, Vendor vendor) {
+        logger.trace("try to update vendor with id=" + id);
+        Session session = sessionFactory.getCurrentSession();
+        Vendor persistedVendor = session.get(Vendor.class, id);
+        persistedVendor.setId(id);
+        persistedVendor.setName(vendor.getName());
+        persistedVendor.setGoods(vendor.getGoods());
+        try {
+            session.persist(persistedVendor);
+            return true;
+        } catch (Exception e) {
+            logger.debug("attempt to update vendor with nonexistent id = " + id);
+            return false;
+        }
+    }
+
+    @Transactional
+    public boolean deleteVendor(Integer id) {
+        Session session = sessionFactory.getCurrentSession();
+        logger.trace("try to delete vendor with id=" + id);
+        try {
+            session.createQuery("delete from Vendor v where v.id = :id")
+                    .setParameter("id", id)
+                    .executeUpdate();
+            return true;
+        } catch (Exception e) {
+            logger.debug("attempt to delete vendor with nonexistent id = " + id);
+            return false;
+        }
+    }
 }
--- src/main/java/com/example/demo/services/VendorService.java
+++ src/main/java/com/example/demo/services/VendorService.java
@@ -1,8 +1,10 @@
 package com.example.demo.services;
 
+import com.example.demo.dao.VendorDao;
 import com.example.demo.entities.Vendor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,47 +13,40 @@ import java.util.List;
 public class VendorService {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    private final VendorDao vendorDao;
+
+    @Autowired
+    public VendorService(VendorDao vendorDao) {
+        this.vendorDao = vendorDao;
+    }
+
 
     public List<Vendor> getAllVendors() {
-        logger.info("attempt to get all vendors");
-//        return vendorDao.findAll();
-        return null;
+        logger.trace("attempt to get all vendors");
+        return vendorDao.getAllVendors();
     }
 
     public Vendor getVendorById(Integer id) {
-        logger.info("attempt to get vendor with id = " + id);
-        return null;
+        logger.trace("attempt to get vendor with id = " + id);
+        return vendorDao.getVendorById(id);
     }
 
     public Integer createVendor(Vendor vendor) {
-        logger.info("attempt to create vendor");
         try {
-            return null;
+            return vendorDao.createVendor(vendor);
         } catch (Exception e) {
             return 0;
         }
     }
 
     public boolean updateVendor(Integer id, Vendor vendor) {
-//        logger.info("attempt to update vendor with id = " + id);
-//        Vendor newVendor = vendorDao.findOne(id);
-//        logger.debug("check if vendor with id " + id + " exists in database");
-//        if (newVendor != null) {
-//            logger.debug("Update vendor with input id = " + id);
-//            vendor.setId(id);
-//            vendorDao.save(vendor);
-//            return true;
-//        } else {
-//            logger.debug("attempt to update vendor with nonexistent id = " + id);
-//            return false;
-//        }
-        return false;
+        return vendorDao.updateVendor(id, vendor);
     }
 
     public boolean deleteVendorById(Integer id) {
         logger.info("attempt to delete vendor with id = " + id);
         try {
-//            vendorDao.delete(id);
+            vendorDao.deleteVendor(id);
             return true;
         } catch (Exception e) {
             return false;
