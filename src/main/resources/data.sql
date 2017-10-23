b8c273e75489c019281100faafb693ad27cabd01
vadimka <zavertalyuk.v@gmail.com>
2017-10-23 16:22:43 +0300

    correct inserts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,7 +8,8 @@ INSERT INTO departments (id, name) VALUES
   (1, 'Board'),
   (2, 'IT'),
   (3, 'HR'),
-  (4, 'Security');
+  (4, 'Security')
+ON CONFLICT DO NOTHING ;
 
 CREATE TABLE IF NOT EXISTS positions (
   id integer PRIMARY KEY DEFAULT nextval('positions'::regclass),
@@ -21,7 +22,8 @@ INSERT INTO positions (id, name) VALUES
   (3, 'Head Of Department'),
   (4, 'Developer'),
   (5, 'HR Manager'),
-  (6, 'Sequrity Guard');
+  (6, 'Sequrity Guard')
+ON CONFLICT DO NOTHING;
 
 CREATE TABLE IF NOT EXISTS employees (
   id integer PRIMARY KEY DEFAULT nextval('employees'::regclass),
@@ -36,15 +38,16 @@ CREATE TABLE IF NOT EXISTS employees (
 
 INSERT INTO employees (id, name,gender,year_of_birth,dep_id,position_id,salary,chief_id) VALUES
   (2, 'Vadimka','M','1961-07-16',2,3,1000,10),
-  (3, 'vlad','M','1961-08-16',4,3,50,10),
+  (3, 'vlad','M','1961-08-16',0,3,50,10),
   (4, 'misha','M','1961-08-16',2,4,1000,2),
   (5, 'sergey','M','1961-10-16',2,4,1000,2),
-  (6, 'bob','M','1961-09-16',4,6,50,3),
-  (7, 'bill','M','1961-12-16',4,6,50,3),
+  (6, 'bob','M','1961-09-16',0,6,50,3),
+  (7, 'bill','M','1961-12-16',0,6,50,3),
   (8, 'marta','W','1961-11-16',2,4,500,2),
   (9, 'roks','W','1961-01-16',1,1,2000,0),
   (10, 'elena','W','1961-02-16',1,2,1500,9),
   (11, 'nikon','M','1961-07-16',3,3,1000,10),
-  (12, 'kostya','M','1961-06-16',1,3,1000,10);
+  (12, 'kostya','M','1961-06-16',1,3,1000,10)
+ON CONFLICT DO NOTHING;
 
 
