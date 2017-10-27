fb99e0f889254dca38c38f87287c7e973d749311
vadimka <zavertalyuk.v@gmail.com>
2017-10-27 10:30:48 +0300

    get all clients

--- src/main/resources/js/employee.js
+++ src/main/resources/js/employee.js
@@ -1,20 +1,16 @@
 $(function(){
-    $.getJSON('http://localhost:8080/SimpleProject/webapi/employees/' + getUrlVars()['id'], function(employee){
+    $.getJSON('http://localhost:8080/client/' + getUrlVars()['id'], function(employee){
     	console.log(employee);
         $('#employee_id').append('<td>' + employee.id + '</td>');
-        $('#employee_first_name').append('<td>' + employee.firstName + '</td>');
-        $('#employee_last_name').append('<td>' + employee.lastName + '</td>');
-        $('#employee_age').append('<td>' + employee.age + '</td>');
-        $('#employee_gender').append('<td>' + employee.gender + '</td>');
-        $('#employee_department').append('<td>' + employee.department.name + '</td>');
-        $('#employee_salary').append('<td>' + employee.salary + '</td>');
+        $('#employee_name').append('<td>' + employee.name + '</td>');
+        $('#employee_discount').append('<td>' + employee.discount + '</td>');
+
     });
 
     function getUrlVars() {
         var vars = [], hash;
         var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-        for(var i = 0; i < hashes.length; i++)
-        {
+        for (var i = 0; i < hashes.length; i++) {
             hash = hashes[i].split('=');
             vars.push(hash[0]);
             vars[hash[0]] = hash[1];
--- src/main/resources/js/index.js
+++ src/main/resources/js/index.js
@@ -1,96 +1,93 @@
 $(function(){
-    $.getJSON('http://localhost:8080/SimpleProject/webapi/employees', function(employees){
-    	console.log(employees);
-        var $employeeTable = $('#employees');
-    	$.each(employees, function(i, employee){
-    		$employeeTable.append('<tr>',
-                '<td>' + employee.id + '</td>',
-    			'<td>' + employee.firstName + '</td>',
-    			'<td>' + employee.lastName + '</td>',
-                '<td>' + employee.department.name + '</td>', 
-    			'<td>' + employee.status + '</td>',
-    			'<td><a href="employee.html?id=' + employee.id + '"><button>Details</button></a></td>',
+    $.getJSON('http://localhost:8080/client', function(clients){
+    	console.log(clients);
+        var $clientTable = $('#clients');
+    	$.each(clients, function(i, client){
+            $clientTable.append('<tr>',
+                '<td>' + client.id + '</td>',
+    			'<td>' + client.name + '</td>',
+                '<td>' + client.discount + '</td>',
                 '</tr>');
             // $employeeTable.append('<tr>');
             // $employeeTable.append('<tr><td>' + employee.id + '</td>');
             // $employeeTable.append('<td>' + employee.firstName + '</td>');
             // $employeeTable.append('<td>' + employee.lastName + '</td>');
-            // $employeeTable.append('<td>' + employee.department.name + '</td>'); 
+            // $employeeTable.append('<td>' + employee.department.name + '</td>');
             // $employeeTable.append('<td>' + employee.status + '</td>');
-            // $employeeTable.append('<td><a href="employee.html?id=' + employee.id + '"><button>Details</button></a></td></tr>');
+            $clientTable.append('<td><a href="client.html?id=' + client.id + '"><button>Details</button></a></td></tr>');
     	});
     });
 
-    //fill department selector with departments
-    $.getJSON('http://localhost:8080/SimpleProject/webapi/departments', function(departments){
-        console.log(departments);
-        var $selectDepartment = $('#selectDepartment');
-        $.each(departments, function(i, department){                                 
-            console.log(department);
-            console.log(JSON.stringify(department));
-            $selectDepartment.append('<option value=\'' + JSON.stringify(department) + '\'>' + department.name + '</option>');
-        });
-    });
+    // //fill department selector with departments
+    // $.getJSON('http://localhost:8080/SimpleProject/webapi/departments', function(departments){
+    //     console.log(departments);
+    //     var $selectDepartment = $('#selectDepartment');
+    //     $.each(departments, function(i, department){
+    //         console.log(department);
+    //         console.log(JSON.stringify(department));
+    //         $selectDepartment.append('<option value=\'' + JSON.stringify(department) + '\'>' + department.name + '</option>');
+    //     });
+    // });
 
-    $.getJSON('http://localhost:8080/SimpleProject/webapi/departments/statistics', function(statistics){
-        console.log(statistics);
-        var $depStat = $('#depStat');
-        $.each(statistics, function(i, stat){
-            $depStat.append('<tr>',
-                '<td>' + stat.department + '</td>',
-                '<td>' + stat.employeesNumber + '</td>',
-                '<td>' + stat.minSalary + '</td>',
-                '<td>' + stat.maxSalary + '</td>', 
-                '<td>' + stat.totalSalary + '</td>',
-                '<td>' + stat.averageSalary + '</td>',
-                '<td>' + stat.womenNumber + '</td>',
-                '</tr>');    
-        });
-    });
+    // $.getJSON('http://localhost:8080/SimpleProject/webapi/departments/statistics', function(statistics){
+    //     console.log(statistics);
+    //     var $depStat = $('#depStat');
+    //     $.each(statistics, function(i, stat){
+    //         $depStat.append('<tr>',
+    //             '<td>' + stat.department + '</td>',
+    //             '<td>' + stat.employeesNumber + '</td>',
+    //             '<td>' + stat.minSalary + '</td>',
+    //             '<td>' + stat.maxSalary + '</td>',
+    //             '<td>' + stat.totalSalary + '</td>',
+    //             '<td>' + stat.averageSalary + '</td>',
+    //             '<td>' + stat.womenNumber + '</td>',
+    //             '</tr>');
+    //     });
+    // });
 
-    $.getJSON('http://localhost:8080/SimpleProject/webapi/employees/statistics', function(statistics){
-        console.log(statistics);
-        var $empStat = $('#empStat');
-        $.each(statistics, function(i, stat){
-            $empStat.append('<tr>',
-                '<td>' + stat.firstName + '</td>',
-                '<td>' + stat.lastName + '</td>',
-                '<td>' + stat.department + '</td>',
-                '<td>' + stat.salary + '</td>',
-                '<td>' + stat.avgSalary + '</td>',
-                '<td>' + stat.avgSalaryInDep + '</td>',
-                '<td>' + stat.avgSalaryByGender + '</td>',
-                '<td>' + stat.avgSalaryInDepByGender + '</td>', 
-                '<td>' + stat.sameGenderInDep + '</td>',
-                '<td>' + stat.menInDep + '</td>',
-                '<td>' + stat.womenInDep + '</td>',
-                '<td>' + stat.menTotal + '</td>',
-                '<td>' + stat.womenTotal + '</td>',
-                '</tr>');    
-        });
-    });
+    // $.getJSON('http://localhost:8080/SimpleProject/webapi/employees/statistics', function(statistics){
+    //     console.log(statistics);
+    //     var $empStat = $('#empStat');
+    //     $.each(statistics, function(i, stat){
+    //         $empStat.append('<tr>',
+    //             '<td>' + stat.firstName + '</td>',
+    //             '<td>' + stat.lastName + '</td>',
+    //             '<td>' + stat.department + '</td>',
+    //             '<td>' + stat.salary + '</td>',
+    //             '<td>' + stat.avgSalary + '</td>',
+    //             '<td>' + stat.avgSalaryInDep + '</td>',
+    //             '<td>' + stat.avgSalaryByGender + '</td>',
+    //             '<td>' + stat.avgSalaryInDepByGender + '</td>',
+    //             '<td>' + stat.sameGenderInDep + '</td>',
+    //             '<td>' + stat.menInDep + '</td>',
+    //             '<td>' + stat.womenInDep + '</td>',
+    //             '<td>' + stat.menTotal + '</td>',
+    //             '<td>' + stat.womenTotal + '</td>',
+    //             '</tr>');
+    //     });
+    // });
 
-    $('form').submit(function(event){
-        var data = $(this).serializeArray();
-        console.log(data);
-        var jsonEmployee = arrayToJson(data);
-        console.log(jsonEmployee);
-        $.ajax({
-            url: "http://localhost:8080/SimpleProject/webapi/employees",
-            type: "POST",
-            data: jsonEmployee,
-            dataType: "json",
-            contentType: "application/json",
-            async: false,
-            success: function() {
-                var msg = 'Employee ' + data[0]['value'] + ' ' + data[1]['value'] + ' has been hired';
-                alert(msg);
-            }
-        });
-        console.log("Employee " + jsonEmployee + " has been posted");
-
-        // event.preventDefault();
-    });
+    // $('form').submit(function(event){
+    //     var data = $(this).serializeArray();
+    //     console.log(data);
+    //     var jsonEmployee = arrayToJson(data);
+    //     console.log(jsonEmployee);
+    //     $.ajax({
+    //         url: "http://localhost:8080/SimpleProject/webapi/employees",
+    //         type: "POST",
+    //         data: jsonEmployee,
+    //         dataType: "json",
+    //         contentType: "application/json",
+    //         async: false,
+    //         success: function() {
+    //             var msg = 'Employee ' + data[0]['value'] + ' ' + data[1]['value'] + ' has been hired';
+    //             alert(msg);
+    //         }
+    //     });
+    //     console.log("Employee " + jsonEmployee + " has been posted");
+    //
+    //     // event.preventDefault();
+    // });
 
     function arrayToJson(array) {
         var object = {};
