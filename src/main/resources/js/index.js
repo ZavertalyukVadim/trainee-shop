9d4f17b49a9f65fe0900c7ac365465206b60347d
vadimka <zavertalyuk.v@gmail.com>
2017-10-30 10:46:43 +0200

    change route to js file

--- src/main/resources/js/index.js
+++ src/main/resources/js/index.js
@@ -1,23 +1,23 @@
 $(function () {
-    $.getJSON('http://localhost:8080/client', function (clients) {
-        console.log(clients);
-
-        var table = document.getElementById("clients");
-        $.each(clients, function (i, client) {
-            var row = table.insertRow();
-
-            var id = row.insertCell(0);
-
-            var name = row.insertCell(1);
-
-            var discount = row.insertCell(2);
-
-            id.innerHTML = client.id;
-            name.innerHTML = '<a href="client.html?id=' + client.id + '">' + client.name + '</a>';
-            discount.innerHTML = client.discount;
-        });
-
-    });
+    // $.getJSON('http://localhost:8080/client', function (clients) {
+    //     console.log(clients);
+    //
+    //     var table = document.getElementById("clients");
+    //     $.each(clients, function (i, client) {
+    //         var row = table.insertRow();
+    //
+    //         var id = row.insertCell(0);
+    //
+    //         var name = row.insertCell(1);
+    //
+    //         var discount = row.insertCell(2);
+    //
+    //         id.innerHTML = client.id;
+    //         name.innerHTML = '<a href="client.html?id=' + client.id + '">' + client.name + '</a>';
+    //         discount.innerHTML = client.discount;
+    //     });
+    //
+    // });
 
 
     // //fill department selector with departments
