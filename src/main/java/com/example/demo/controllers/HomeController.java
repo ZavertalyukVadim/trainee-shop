a16befbf005f7c9ed21d8db46f59ddc3f80cfee2
vadimka <zavertalyuk.v@gmail.com>
2017-10-30 11:37:03 +0200

    add annotation cross origin

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -3,13 +3,14 @@ package com.example.demo.controllers;
 import com.example.demo.entities.Type;
 import com.example.demo.services.TypesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-
+@CrossOrigin
 public class HomeController {
     private final TypesService typesService;
 
