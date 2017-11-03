a26b44883da7993af8b21d80e4e82245219adad3
vadimka <zavertalyuk.v@gmail.com>
2017-11-03 10:40:03 +0200

    add commit and each method

--- src/main/java/com/example/demo/dao/OrderItemDaoInJbdc.java
+++ src/main/java/com/example/demo/dao/OrderItemDaoInJbdc.java
@@ -71,125 +71,119 @@ public class OrderItemDaoInJbdc {
 
         String sql = "SELECT * FROM order_items JOIN goods ON goods.id=order_items.goods_id  JOIN vendor ON vendor.id = goods.vendor_id";
 
-        Connection conn = null;
+
         List<OrderItem> orderItems = new ArrayList<>();
-        try {
-            conn = dataSource.getConnection();
-            PreparedStatement ps = conn.prepareStatement(sql);
-            Type[] types = Type.values();
-            OrderItem orderItem = null;
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                orderItem = new OrderItem(
-                        rs.getInt(1),
-                        rs.getInt("count"),
-                        new Goods(
-                                rs.getInt(3),
-                                rs.getString("name"),
-                                rs.getBigDecimal("price"),
-                                types[Integer.parseInt(rs.getString("type"))],
-                                new Vendor(rs.getInt(10),
-                                        rs.getString(11))
-                        )
-                );
-                orderItems.add(orderItem);
-            }
-            rs.close();
-            ps.close();
-            return orderItems;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally {
-            if (conn != null) {
-                try {
-                    conn.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
+        try (Connection conn = dataSource.getConnection()) {
+            try {
+                conn.setAutoCommit(false);
+                PreparedStatement ps = conn.prepareStatement(sql);
+                Type[] types = Type.values();
+                OrderItem orderItem = null;
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    orderItem = new OrderItem(
+                            rs.getInt(1),
+                            rs.getInt("count"),
+                            new Goods(
+                                    rs.getInt(3),
+                                    rs.getString("name"),
+                                    rs.getBigDecimal("price"),
+                                    types[Integer.parseInt(rs.getString("type"))],
+                                    new Vendor(rs.getInt(10),
+                                            rs.getString(11))
+                            )
+                    );
+                    orderItems.add(orderItem);
                 }
+                rs.close();
+                ps.close();
+                conn.commit();
+                return orderItems;
+            } catch (SQLException e) {
+                conn.rollback();
+                e.printStackTrace();
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+        return orderItems;
     }
 
 
     public boolean delete(Integer id) {
         String sql = "DELETE FROM order_items WHERE id = ?";
-        Connection conn = null;
-        try {
-            conn = dataSource.getConnection();
-            PreparedStatement ps = conn.prepareStatement(sql);
-            ps.setInt(1, id);
-            ps.executeUpdate();
-            ps.close();
-            return true;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
 
-        } finally {
-            if (conn != null) {
-                try {
-                    conn.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
+        try (Connection conn = dataSource.getConnection()) {
+            conn.setAutoCommit(false);
+            try {
+                PreparedStatement ps = conn.prepareStatement(sql);
+                ps.setInt(1, id);
+                ps.executeUpdate();
+                ps.close();
+                conn.commit();
+                return true;
+            } catch (SQLException e) {
+                conn.rollback();
+                e.printStackTrace();
+
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+        return false;
     }
 
     public boolean update(OrderItem orderItem) {
         String sql = "UPDATE order_items SET count =?,goods_id =? WHERE id = ?";
-        Connection conn = null;
 
-        try {
-            conn = dataSource.getConnection();
-            PreparedStatement ps = conn.prepareStatement(sql);
-            ps.setInt(1, orderItem.getCount());
-            ps.setInt(2, orderItem.getGoods().getId());
-            ps.setInt(3, orderItem.getId());
-            ps.executeUpdate();
-            ps.close();
-            return true;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-
-        } finally {
-            if (conn != null) {
-                try {
-                    conn.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
+        try (Connection conn = dataSource.getConnection()) {
+            try {
+                conn.setAutoCommit(false);
+                PreparedStatement ps = conn.prepareStatement(sql);
+                ps.setInt(1, orderItem.getCount());
+                ps.setInt(2, orderItem.getGoods().getId());
+                ps.setInt(3, orderItem.getId());
+                ps.executeUpdate();
+                ps.close();
+                conn.commit();
+                return true;
+            } catch (SQLException e) {
+                conn.rollback();
+                e.printStackTrace();
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+        return false;
+
     }
 
     public Integer create(OrderItem orderItem) {
         String sql = "INSERT INTO order_items(count,goods_id) VALUES (?,?) RETURNING id";
         Integer id = 0;
-        Connection conn = null;
-        try {
-            conn = dataSource.getConnection();
-            PreparedStatement ps = conn.prepareStatement(sql);
-            ps.setInt(1, orderItem.getCount());
-            ps.setInt(2, orderItem.getGoods().getId());
+        try (Connection conn = dataSource.getConnection()) {
+            try {
+                conn.setAutoCommit(false);
+                PreparedStatement ps = conn.prepareStatement(sql);
+                ps.setInt(1, orderItem.getCount());
+                ps.setInt(2, orderItem.getGoods().getId());
 
-            ResultSet resultSet = ps.executeQuery();
-            System.out.println(resultSet);
-            if (resultSet.next()) {
-                id = resultSet.getInt("id");
-            }
-            ps.close();
-            return id;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-
-        } finally {
-            if (conn != null) {
-                try {
-                    conn.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
+                ResultSet resultSet = ps.executeQuery();
+                System.out.println(resultSet);
+                if (resultSet.next()) {
+                    id = resultSet.getInt("id");
                 }
+                ps.close();
+                conn.commit();
+                return id;
+            } catch (SQLException e) {
+                conn.rollback();
+                e.printStackTrace();
+
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+        return 0;
     }
 }
